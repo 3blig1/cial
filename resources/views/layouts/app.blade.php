2d887463fafa6a3791d24e68c8eea41c44018db9
3blig1 <koguegilbert@gmail.com>
2026-05-16 13:39:56 +0200

    feat: add logo favicon to browser tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Formation')</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo/Logo_icone.png') }}">
     <script src="https://cdn.tailwindcss.com/3.4.16"></script>
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet">
